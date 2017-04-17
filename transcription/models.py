3445a521ff30d382846126b16731940ec60dc8cd
James Flynn <james@samsungaccelerator.com>
2017-04-17 11:19:07 +0700

    checkbox

--- transcription/models.py
+++ transcription/models.py
@@ -29,7 +29,7 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 	MTurkID = models.CharField()
-	paymentOK = models.BooleanField(widget=widgets.CheckboxInput(check_test=True))
+	paymentOK = models.BooleanField(widget=widgets.CheckboxInput())
 	neverWorked = models.BooleanField()
 	yearBorn = models.PositiveIntegerField()
 	gender = models.CharField()
