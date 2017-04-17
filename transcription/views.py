f895891657aee06feb35494148ae16a8ffff5a1a
James Flynn <james@samsungaccelerator.com>
2017-04-17 15:59:23 +0700

    manager complete

--- transcription/views.py
+++ transcription/views.py
@@ -13,6 +13,26 @@ class MyPage(Page):
     form_model = models.Player
     form_fields = ['MTurkID', 'paymentOK', 'neverWorked', 'yearBorn', 'gender']
 
+class MyPage2(Page):
+    form_model = models.Player
+    form_fields = ['experience', 'transExp', 'eduLevel', 'dailyHHEarn']
+
+class MyPage3(Page):
+    form_model = models.Player
+    form_fields = ['howLong']
+
+class MyPage4(Page):
+	pass
+
+class MyPage5(Page):
+    form_model = models.Player
+    form_fields = ['pref1','pref2','pref3','pref4','pref5']
+
+class MyPage6(Page):
+    form_model = models.Player
+    form_fields = ['bid']
+
+#    form_fields = ['experience', 'transExp']
 
 #class ResultsWaitPage(WaitPage):
 
@@ -28,6 +48,11 @@ page_sequence = [
 	Introduction,
 	Introduction2,
     MyPage,
+    MyPage2,
+    MyPage4,
+    MyPage3,
+    MyPage5,
+    MyPage6,
 #    ResultsWaitPage,
     Results
 ]
