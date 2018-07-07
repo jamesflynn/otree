a2d32b8289a7aa19f907e80e7cd7e9a8fdf9d4f9
James Flynn <james@samsungaccelerator.com>
2018-07-07 09:44:59 -0400

    fix quals

--- settings.py
+++ settings.py
@@ -127,25 +127,25 @@ mturk_hit_settings = {
         {
             'QualificationTypeId': "3NF07PFA1K2EGWJFNBQ4JN81TXKETV",   # Prevent Repeats PRODUCTION
             'Comparator': "DoesNotExist",
-            'ActionsGuarded': "RequiredToPreview"
+            'RequiredToPreview': True
         },
         {
             'QualificationTypeId': "00000000000000000071",    # Worker_Locale
             'Comparator': "EqualTo",
             'LocaleValues': [{'Country': "US"}],
-            'ActionsGuarded': "RequiredToPreview"
+            'RequiredToPreview': True
         },
         {
             'QualificationTypeId': "000000000000000000L0",    # Worker_PercentAssignmentsApproved
             'Comparator': "GreaterThan",
             'IntegerValues': [50],
-            'ActionsGuarded': "RequiredToPreview"
+            'RequiredToPreview': True
         },
         {
             'QualificationTypeId': "00000000000000000040",    # Worker_NumberHitsApproved
             'Comparator': "GreaterThanOrEqualTo",
             'IntegerValues': [100],
-            'ActionsGuarded': "RequiredToPreview"
+            'RequiredToPreview': True
         },
 #        {
 #            'QualificationTypeId': "2ARFPLSP75KLA8M8DH1HTEQVJT3SY6",    # Masters Sandbox
